ecd6322b1f17a6b72ea2896c87ec551d2d49a808
chamodya <chamodyarajapaksha1@gmail.com>
2025-03-31 20:46:54 +0530

    check username and password with app. props and inmemory

--- src/main/java/com/security/project/config/ProjectSecurityConfig.java
+++ src/main/java/com/security/project/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.security.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,4 +30,14 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin= User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
 }
