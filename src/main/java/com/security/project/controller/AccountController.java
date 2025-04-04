649183f70dff6d9e42941bd4e8f34a0b1beba5bb
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-04 12:48:05 +0530

    check with Preauthorize with role base

--- src/main/java/com/security/project/controller/AccountController.java
+++ src/main/java/com/security/project/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.security.project.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 public class AccountController {
-    @Secured("ROLE_ADMIN")
+//    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_USER')") //in preauthorize check this statement before execute method
     @GetMapping("/my-account")
     public String getAccountDetails(){
         return "this is for acc details";
