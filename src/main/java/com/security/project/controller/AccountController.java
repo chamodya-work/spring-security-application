410e08ac1aee262fc71161041c3e929c70d89e15
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-04 13:07:08 +0530

    checked with postAuthorize to acc Controller endpoint

--- src/main/java/com/security/project/controller/AccountController.java
+++ src/main/java/com/security/project/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.security.project.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AccountController {
 //    @Secured("ROLE_ADMIN")
-    @PreAuthorize("hasAuthority('ROLE_USER')") //in preauthorize check this statement before execute method
+//    @PreAuthorize("hasAuthority('ROLE_USER')") //in preauthorize check this statement before execute method
+    @PostAuthorize("hasAuthority('ROLE_USER')") //in postAuthorize it first going though method executed and  before retun statment
+    //it check the authorize.so that why we can see print in method for unauthorized users when accessing this end point
     @GetMapping("/my-account")
     public String getAccountDetails(){
+        System.out.println("inside the method");
         return "this is for acc details";
     }
 }
